33a86cfdfedcdf34a02e5e71adcf1349117380cf
Harshit <harshit12910@gmail.com>
2025-05-02 01:29:30 +0530

    feat: Move checked tasks to bottom and restore position when unchecked

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const addButton = document.querySelector(".add-task");
 const userInput = document.getElementById("add-input");
 const itemlist = document.querySelector(".list");
+let currentIndex = 0;
+
 
 
 addButton.addEventListener("click", function () {
@@ -8,21 +10,39 @@ addButton.addEventListener("click", function () {
 
   if (value !== "") {
     const li = document.createElement('li');
-    li.innerHTML = `<input class="checkbox" type="checkbox"><span class="task-text">${value}</span>`;  
+    li.innerHTML = `<input class="checkbox" type="checkbox"><span class="task-text">${value}</span>`;
     itemlist.prepend(li);
-    userInput.value = "";
-    console.log("button clicked");
+    userInput.value = "";  
+    li.dataset.index = currentIndex++; // store original position
+
 
     const checkbox = li.querySelector(".checkbox");
     const span = li.querySelector(".task-text");
 
-    checkbox.addEventListener('change', () => {
-    console.log('Checkbox checked:', checkbox.checked);
-  
-    span.classList.toggle("done", checkbox.checked);
-});
-
+    checkbox.addEventListener('change', function () {
+      span.classList.toggle("done", this.checked);
     
+      if (this.checked) {
+        itemlist.appendChild(li);
+      } else {
+        const originalIndex = parseInt(li.dataset.index); // lowercase 'index'
+        const items = Array.from(itemlist.children);
+    
+        let inserted = false;
+        for (let i = 0; i < items.length; i++) {
+          const otherIndex = parseInt(items[i].dataset.index);
+          if (originalIndex < otherIndex) {
+            itemlist.insertBefore(li, items[i]);
+            inserted = true;
+            break;
+          }
+        }
+        if (!inserted) itemlist.appendChild(li);
+      }
+    });
+    
+
+  itemlist.prepend(li);
   } else {
     alert("Please Enter Something!")
     console.log("user didn't put anything");

23fbbb4a32d1e0a36674b0742c1ddfb19485eaeb
Harshit <harshit12910@gmail.com>
2025-05-01 15:33:32 +0530

    Add: Checkbox toggle to mark tasks as done with strikethrough

--- script.js
+++ script.js
@@ -2,15 +2,26 @@ const addButton = document.querySelector(".add-task");
 const userInput = document.getElementById("add-input");
 const itemlist = document.querySelector(".list");
 
+
 addButton.addEventListener("click", function () {
   const value = userInput.value;
 
   if (value !== "") {
     const li = document.createElement('li');
-    li.textContent = value;
-    itemlist.appendChild(li);
-    userInput = "";
+    li.innerHTML = `<input class="checkbox" type="checkbox"><span class="task-text">${value}</span>`;  
+    itemlist.prepend(li);
+    userInput.value = "";
     console.log("button clicked");
+
+    const checkbox = li.querySelector(".checkbox");
+    const span = li.querySelector(".task-text");
+
+    checkbox.addEventListener('change', () => {
+    console.log('Checkbox checked:', checkbox.checked);
+  
+    span.classList.toggle("done", checkbox.checked);
+});
+
     
   } else {
     alert("Please Enter Something!")
@@ -24,4 +35,7 @@ userInput.addEventListener("keydown", function(event) {
     console.log("User pressed enter");
     
   }
-})
+});
+
+
+
